2aec5818a48465bddd0e8bdbed5dcfea86b9bf20
TheEntropy1 <prakhar799000@gmail.com>
2025-05-10 16:32:39 +0530

    Update type.ts

--- marlin/client/src/type.ts
+++ marlin/client/src/type.ts
@@ -1,3 +1,4 @@
+// client/src/types.ts
 export interface Thread {
   id: string;
   boardId: string;
@@ -7,4 +8,11 @@ export interface Thread {
   views: number;
   created_at: string;
   image_url?: string;
+}
+
+export interface Board {
+  id: string;
+  name: string;
+  description: string;
+  is_nsfw: boolean;
 }
